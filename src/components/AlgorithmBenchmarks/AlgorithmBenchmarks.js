c6a37e9c9f5c29059d276eaff8992d9483325126
Daniel Birnbaum <danielbirnbaum@dhcp-10-105-150-244.wireless.northwestern.private>
2018-08-14 10:54:10 -0500

    no

--- src/components/AlgorithmBenchmarks/AlgorithmBenchmarks.js
+++ src/components/AlgorithmBenchmarks/AlgorithmBenchmarks.js
@@ -1,26 +1,21 @@
 import React, { Component } from 'react';
+
 import 'bootstrap/dist/css/bootstrap.css';
+
 import '../Assignments/Assignments.css';
 
 class AlgorithmBenchmarks extends Component {
     constructor(props) {
         super(props);
+
         this.state = {
-            value: null,
             changed: false,
+            value: null,
         };
 
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
-
-componentDidMount() {
-    if (!this.state.value) {
-        this.setState({
-            value: this.props.value,
-        })
-    }
-}
 
     handleChange(event) {
         this.setState({
@@ -33,12 +28,20 @@ componentDidMount() {
         event.preventDefault();
     }
 
+    componentDidMount() {
+        if (!this.state.value) {
+            this.setState({
+                value: this.props.value,
+            })
+        }
+    }
+
     render() {
         return (
             <div>
                 <form onSubmit={this.handleSubmit}>
                     <label>
-                        <input type="number" value={this.state.changed?this.state.value:""} onChange={this.handleChange} min={this.props.min?this.props.min:null} max={this.props.max?this.props.max:null} step={this.props.step?this.props.step:null} placeholder={this.props.placeholder}/>
+                        <input type="number" value={this.state.changed ? this.state.value : ""} onChange={this.handleChange} min={this.props.min ? this.props.min : null} max={this.props.max ? this.props.max : null} step={this.props.step ? this.props.step : null} placeholder={this.props.placeholder} />
                     </label>
                 </form>
             </div>
